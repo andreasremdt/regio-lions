cebbb183630f280a63908430ee858c4dc13be1e7
Andreas Remdt <me@andreasremdt.com>
2024-02-02 21:54:21 +0100

    feat: make homepage posts dynamic

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,18 @@
+import { getCollection } from "astro:content";
+
 export function formatDate(date: Date) {
   return new Intl.DateTimeFormat("de-DE", {
     day: "2-digit",
     month: "long",
     year: "numeric",
   }).format(date);
+}
+
+export async function getNews(limit = Infinity) {
+  const news = await getCollection("news");
+
+  return news
+    .filter((entry) => Boolean(entry.body))
+    .sort((a, b) => b.data.created_at.getTime() - a.data.created_at.getTime())
+    .slice(0, limit);
 }
