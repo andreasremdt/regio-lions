79a0cd8878aae81576fe849271da1ef0ad556e60
Andreas Remdt <me@andreasremdt.com>
2024-02-02 21:43:38 +0100

    feat: improve image handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false })],
+  image: {
+    domains: ["ik.imagekit.io"],
+  },
 });
