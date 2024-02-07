64971e8270d069c2af309c14a5650842802e2abd
Andreas Remdt <me@andreasremdt.com>
2024-02-07 21:03:43 +0100

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   output: "hybrid",
   image: {
     domains: ["ik.imagekit.io"],
   },
+  adapter: vercel(),
 });
