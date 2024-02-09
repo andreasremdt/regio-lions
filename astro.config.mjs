e8f8244ca4b8cb02dc515475a9a6536dacdf6e8e
Andreas Remdt <me@andreasremdt.com>
2024-02-09 20:16:34 +0100

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://regio-lions.com",
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
   output: "hybrid",
   image: {
