409c739acca3e65d8f1ce3e9a4df065f857f9039
Andreas Remdt <me@andreasremdt.com>
2025-09-08 13:18:26 +0200

    chore: update main packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://regio-lions.com",
-  prefetch: {
-    prefetchAll: true,
-  },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-  ],
-  output: "hybrid",
-  image: {
-    domains: ["ik.imagekit.io"],
-  },
+  prefetch: { prefetchAll: true },
+  integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+  output: "server",
+  image: { domains: ["ik.imagekit.io"] },
   adapter: vercel(),
 });
